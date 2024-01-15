f210c576b85e00b4d11a7f34e85efc32de6bb690
민승 김 <rlaalstmd0606@gmail.com>
2024-01-15 16:46:54 +0900

    찐 커밋

--- answer/ConsoleEngine/ConsoleObject.h
+++ answer/ConsoleEngine/ConsoleObject.h
@@ -4,38 +4,38 @@
 // 전방선언 
 class ConsoleObject
 {
-	friend class EngineCore;
+	friend class EngineCore; // friend : EngineCore의 private를 사용하겠다.
 
 public:
-	ConsoleObject();
-	ConsoleObject(const int2& _StartPos, char _RenderChar);
+	ConsoleObject(); // 기본 생성자
+	ConsoleObject(const int2& _StartPos, char _RenderChar); // 초기화를 이용한 생성자
 	// 소멸자 virtual 
-	virtual ~ConsoleObject();
+	virtual ~ConsoleObject(); // 가상 소멸자
 
-	int2 GetPos() const;
-	char GetRenderChar() const;
+	int2 GetPos() const; // this -> const 2개 사용 -> 위치, 값 변경 불가능
+	char GetRenderChar() const; // this -> const 2개 사용 -> 위치, 값 변경 불가능
 
-	inline void SetRenderChar(char _Ch)
+	inline void SetRenderChar(char _Ch) // 몬스터, 플레이어, 총알 랜더링 표시할 것 set -> 받아서 저장
 	{
 		RenderChar = _Ch;
 	}
 
-	inline void SetPos(const int2& _Pos)
+	inline void SetPos(const int2& _Pos) //  const : 본연의 값 변경X, Set -> 몬스터, 플레이어, 총알 위치 값 받아오기
 	{
 		Pos = _Pos;
 	}
 
-	inline void AddPos(const int2& _Dir)
+	inline void AddPos(const int2& _Dir) // AddPos 기존의 위치값에 up, down, left, right -> 한칸 씩 이동
 	{
 		Pos += _Dir;
 	}
 
-	virtual void Update()
+	virtual void Update() // 가상함수 -> 자식 함수 오버라이드 참고 즉 부모클래스
 	{
 
 	}
 
-	void Destroy()
+	void Destroy() // 기능 아직 확인불가
 	{
 		DeathValue = true;
 	}
